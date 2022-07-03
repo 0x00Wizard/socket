bca26e8b2c6545caf8d20098465c47ae8bfa086a
wizardxxdx <101555271+wizardxxdx@users.noreply.github.com>
2022-07-03 10:28:49 -0700

    port scanner.py

--- scanner.py
+++ scanner.py
@@ -4,3 +4,17 @@ import sys
 import socket
 from datetime import datetime
 
+# Define the target
+
+if len(sys.argv) == 2:
+    target = socket.gethostname(sys.argv[1])
+else:
+    print("Invalid amount of arguments.")
+    print("Syntax: Python3 scanner.py <ip>")
+
+
+# Add a pretty banner
+
+print("-" * 50)
+print(f"Scanning target " + target)
+print("Time started: ")
